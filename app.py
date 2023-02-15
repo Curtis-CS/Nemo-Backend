9d6b20991adec5d2ce6c9ee4ca6a1213a8e1a9b1
Curtis-CS <cburchfield@nevada.unr.edu>
2023-02-14 21:23:13 -0800

    Curtis Flask Backend V4

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-
 @app.route("/", methods=['POST'])
 def process_image():
     if request.method == "POST":
@@ -15,4 +14,5 @@ def process_image():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    print("This is a test")
+    app.run()
