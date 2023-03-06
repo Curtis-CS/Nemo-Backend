81d7a0cdcc9eaf81c7dbd612c7d623cab6799d1f
Curtis-CS <cburchfield@nevada.unr.edu>
2023-03-05 20:12:11 -0800

    Curtis Base64 Images Switch Initial

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 from flask_cors import CORS
-
+import base64
 app = Flask(__name__)
 CORS(app)
 
@@ -8,10 +8,14 @@ CORS(app)
 @app.route("/", methods=['POST'])
 def process_image():
     if request.method == "POST":
-        form_data = request.get_data()
-        app.logger.info(form_data)
-        return "Hello World!"
-
+        image_data = request.json.get('imageB64Data')
+        theData = str(image_data)
+        theData = theData.replace('data:image/jpeg;base64,', '')
+        app.logger.warning(theData)
+        image_binary = base64.b64decode(theData)
+        with open('image.jpg', 'wb') as f:
+            f.write(image_binary)
+        return "Data Recieved"
 
 if __name__ == "__main__":
     app.run(debug=True)
