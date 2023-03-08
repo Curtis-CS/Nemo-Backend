7ab028b55367a9df718aeef8672f3f8f48acb462
Curtis-CS <cburchfield@nevada.unr.edu>
2023-03-07 16:43:52 -0800

    Curtis .gitignore

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 from flask_cors import CORS
-
+import base64
 app = Flask(__name__)
 CORS(app)
 
@@ -8,10 +8,20 @@ CORS(app)
 @app.route("/", methods=['POST'])
 def process_image():
     if request.method == "POST":
-        form_data = request.get_data()
-        app.logger.info(form_data)
-        return "Hello World!"
-
+        image_data = request.json.get('imageB64Data')
+        theData = str(image_data)
+        theData = theData.replace('data:image/jpeg;base64,', '')
+        app.logger.warning(theData)
+        image_binary = base64.b64decode(theData)
+        with open('image.jpg', 'wb') as f:
+            f.write(image_binary)
+        # print("Testing Nemo")
+        # subprocess.Popen(["python3", "./NemoModel/detr/test.py", 
+        #                 "--data_path", "./NemoModel/Images/ImagesToRun/", 
+        #                 "--resume", "./NemoModel/Nemo-DETR-dg.pth", 
+        #                 "--output_dir", "./NemoModel/Images/ProcessedImages/", 
+        #                 "--device" , "cpu", "--disp" ,"1"]).wait()
+        return "Data Recieved"
 
 if __name__ == "__main__":
     app.run(debug=True)
