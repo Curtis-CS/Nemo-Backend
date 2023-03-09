d9cf2f70545a1c840123999fd093bf4062ce10f9
Curtis-CS <cburchfield@nevada.unr.edu>
2023-03-08 19:22:27 -0800

    Curtis Multiple File Uplads V9999

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 from flask_cors import CORS
-import base64
+from PIL import Image
 import subprocess
 app = Flask(__name__)
 CORS(app)
@@ -9,20 +9,27 @@ CORS(app)
 @app.route("/", methods=['POST'])
 def process_image():
     if request.method == "POST":
-        image_data = request.json.get('imageB64Data')
-        theData = str(image_data)
-        theData = theData.replace('data:image/jpeg;base64,', '')
-        app.logger.warning(theData)
-        image_binary = base64.b64decode(theData)
-        with open('image.jpg', 'wb') as f:
-            f.write(image_binary)
+        file = request.files['file']
+        filename = file.filename
+        image = Image.open(file)
+        image.save(filename)
         # print("In Testing Nemo")
         # subprocess.Popen(["python3", "./NemoModel/detr/test.py", 
         #                 "--data_path", "./NemoModel/Images/ImagesToRun/", 
         #                 "--resume", "./NemoModel/Nemo-DETR-dg.pth", 
         #                 "--output_dir", "./NemoModel/Images/ProcessedImages/", 
         #                 "--device" , "cpu", "--disp" ,"1"]).wait()
-        return "Data Recieved"
+        return "Data Recieved, file Saved"
+
+#Function to get all of the files Nemo Processed into an array
+def GetProcessedImages():
+    print("Getting Nemo Results")
+    results = []
+    return results
+
+#Function to clean up the files so Nemo can run smoothly again
+def CleanUpNemorun():
+    print("Cleaning up Nemo Run")
 
 if __name__ == "__main__":
     app.run(debug=True)
