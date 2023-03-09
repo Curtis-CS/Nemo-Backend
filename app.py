64a8f8ed7b593e7514d129aac24b5c91f615d06c
Curtis-CS <cburchfield@nevada.unr.edu>
2023-03-08 22:44:52 -0800

    Curtis Nemo Returning Zip File

--- app.py
+++ app.py
@@ -1,35 +1,69 @@
-from flask import Flask, request
+from flask import Flask, request, send_file
 from flask_cors import CORS
 from PIL import Image
 import subprocess
+import os
+from pathlib import Path
+import io
+import zipfile
 app = Flask(__name__)
 CORS(app)
-
 
 @app.route("/", methods=['POST'])
 def process_image():
     if request.method == "POST":
         file = request.files['file']
+        filesLeft = int(request.form['filesLeft']) - 1
+        app.logger.warning(filesLeft)
         filename = file.filename
         image = Image.open(file)
-        image.save(filename)
-        # print("In Testing Nemo")
-        # subprocess.Popen(["python3", "./NemoModel/detr/test.py", 
-        #                 "--data_path", "./NemoModel/Images/ImagesToRun/", 
-        #                 "--resume", "./NemoModel/Nemo-DETR-dg.pth", 
-        #                 "--output_dir", "./NemoModel/Images/ProcessedImages/", 
-        #                 "--device" , "cpu", "--disp" ,"1"]).wait()
-        return "Data Recieved, file Saved"
+
+        p = Path('./NemoModel/Images/ImagesToRun/' + filename)
+        image.save(p)
+        if (filesLeft < 1):
+            zipFile = RunNemo()
+            headers = {
+                'Content-Disposition': 'filename=files.zip',
+                'Content-Type': 'application/zip'
+            }
+            return send_file(zipFile, mimetype='application/zip', as_attachment=True, download_name="results.zip")
+        return 'null'
+
+# Run Nemo
+def RunNemo():
+    print ("Running Nemo")
+    print("In Testing Nemo")
+    subprocess.Popen(["python3", "./NemoModel/detr/test.py", 
+                    "--data_path", "./NemoModel/Images/ImagesToRun/", 
+                    "--resume", "./NemoModel/Nemo-DETR-dg.pth", 
+                    "--output_dir", "./NemoModel/Images/ProcessedImages/", 
+                    "--device" , "cpu", "--disp" ,"1"]).wait()
+    zipFile = GetProcessedImages()
+    CleanUpNemorun()
+    return zipFile
+    
 
 #Function to get all of the files Nemo Processed into an array
 def GetProcessedImages():
     print("Getting Nemo Results")
-    results = []
-    return results
+    processedImagesDir = './NemoModel/Images/ProcessedImages/Inferences-ImagesToRun'
+    zipFileToSend = io.BytesIO()
+    with zipfile.ZipFile(zipFileToSend, mode="w") as zipFile:
+        for fileName in os.listdir(processedImagesDir):
+            curFilePath = os.path.join(processedImagesDir, fileName)
+            zipFile.write(curFilePath, arcname=fileName)
+    zipFileToSend.seek(0)
+    return zipFileToSend
 
 #Function to clean up the files so Nemo can run smoothly again
 def CleanUpNemorun():
     print("Cleaning up Nemo Run")
+    processedImagesDir = './NemoModel/Images/ProcessedImages/Inferences-ImagesToRun'
+    for file in os.listdir(processedImagesDir):
+        os.remove(os.path.join(processedImagesDir, file))
+    imagesToRunDir = './NemoModel/Images/ImagesToRun'
+    for file in os.listdir(imagesToRunDir):
+        os.remove(os.path.join(imagesToRunDir, file))
 
 if __name__ == "__main__":
     app.run(debug=True)
